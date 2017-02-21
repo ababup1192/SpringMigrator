3f2c8dc29d64b20f5e24f572053ba9050965aceb
abab <ababup1192@gmail.com>
2017-02-21 13:20:40 +0900

    Static import

--- src/test/java/org/ababup1192/MemberMigrationTest.java
+++ src/test/java/org/ababup1192/MemberMigrationTest.java
@@ -20,6 +20,7 @@ import java.time.ZoneId;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.mockito.Mockito.when;
